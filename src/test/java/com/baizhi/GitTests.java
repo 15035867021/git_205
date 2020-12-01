4af7b906f347e385302e6136440dd6231dfcb2f7
zhangxh <zhang_xh624@163.com>
2020-12-01 21:07:04 +0800

    添加一个测试类

---
+++ src/test/java/com/baizhi/GitTests.java
@@ -0,0 +1,18 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("我是一只只");
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+
+}
