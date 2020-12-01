78468df3cd37bb6897e9867f6d479a0521b70e5a
wuwp <wuwp1222@163.com>
2020-12-01 22:23:58 +0800

    这是吴文鹏提交的

---
+++ src/test/java/com/baizhi/GitwWP.java
@@ -0,0 +1,37 @@
+package com.baizhi;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitwWP {
+
+    @Test
+    void contextLoads() {
+        System.out.println("我是一只只");
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+        System.out.println("111");
+
+        int bbb = 456789;
+        int shus = 789456;
+        System.out.println("shjihnsh");
+
+        int ttt = 456789;
+
+        System.out.println("2222");
+
+        System.out.println(",,,,");
+
+        System.out.println("3333");
+
+        System.out.println("....");
+
+        System.out.println("这是wwp上传的东西");
+    }
+
+}
