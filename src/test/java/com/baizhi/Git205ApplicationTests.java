af7e682f7f749a05c3e9d8b87b720393653f097d
zhangxh <zhang_xh624@163.com>
2020-12-01 21:05:17 +0800

    测试类添加一个变量

--- src/test/java/com/baizhi/Git205ApplicationTests.java
+++ src/test/java/com/baizhi/Git205ApplicationTests.java
@@ -9,6 +9,10 @@ class Git205ApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("我是一只只");
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
     }
 
 }
